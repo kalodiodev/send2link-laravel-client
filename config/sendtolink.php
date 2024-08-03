2776abbad2f06cc1b279b540a6c3f5024b48cd84
Athanasios Raptodimos <thanosra@gmail.com>
2024-08-03 13:37:37 +0300

    fix default server url

--- config/sendtolink.php
+++ config/sendtolink.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'server' => env('SEND2LINK_SERVER', 'https://2ln.eu'),
+    'server' => env('SEND2LINK_SERVER', 'https://send2link.eu'),
     'authorization_key' => env("SEND2LINK_AUTH_KEY", ''),
 ];
